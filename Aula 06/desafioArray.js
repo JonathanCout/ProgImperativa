1107797619db0010184874ab4d287752dee13941
JonathanCout <jqcoutinho.eng@gmail.com>
2021-06-11 11:24:45 -0300

    "adicionando splice"

--- Aula 06/desafioArray.js
+++ Aula 06/desafioArray.js
@@ -1,11 +1,11 @@
 //Desafio Array do PG - Lista de Compras 
 
-let listaCompras = ['banana','leite','biscoito','macarrão']
+let listaCompras = ['banana','leite','biscoito','suco','queijo','macarrão']
 console.log('Array inicial:')
 console.log(listaCompras)
 console.log('-----------------------------------------')         //Console.log só para emular pular uma linha
 //Criando uma lista no papel
-let coisasMaisImportantes = listaCompras.join()
+let coisasMaisImportantes = listaCompras.join(' - ')
 console.log('O método join criará uma lista com os objetos do array')
 console.log(coisasMaisImportantes)
 console.log('-----------------------------------------')
@@ -27,5 +27,10 @@ console.log('-----------------------------------------')
 //Comando unshift
 console.log('O método unshift adicionará um dado do array na primeira posição, no caso laranja')
 listaCompras.unshift('laranja')
+console.log(listaCompras)
+console.log('-----------------------------------------')
+//Comando splice
+console.log('O método splice retira um ou mais elementos do array, a partir da posição informada, neste caso a partir do índice 2 ele removerá 2 dados')
+listaCompras.splice(2,2)
 console.log(listaCompras)
 
