df1bdc5635ae1b58f0822780964a8509d38a5911
JonathanCout <jqcoutinho.eng@gmail.com>
2021-06-11 09:01:02 -0300

    "ajustando as funções, adicionando comentários"

--- Aula 05/ex03podesubir.js
+++ Aula 05/ex03podesubir.js
@@ -1,17 +1,18 @@
-//
+//Exercicio PodeSubir
 function podeSubir(altura,acompanhada) {
+    // Condição padrão
     if (altura > 1.4 && altura < 2){
         return "Pode subir"
-
-    } else if (altura >= 1.2 && altura < 1.4 && acompanhada == "sim" ) {
+    // Condição com acompanhante
+    } else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "sim" ) {
         return "Pode subir por estar acompanhada"
-
-    } else if (altura >= 1.2 && altura < 1.4 && acompanhada == "não") {
+    // Condição sem acompanhante
+    } else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "não") {
         return "Necessita de acompanhante para subir"
-
+    // Condição falha para <1.2m e >2m
     } else {
         return "Não pode subir"
     } 
 }
 
-console.log(podeSubir(1.2,"não"))
+console.log(podeSubir(1.3,"sim"))
