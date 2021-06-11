242b0b9cee011fa54a81188429dfc1251f8669f0
JonathanCout <jqcoutinho.eng@gmail.com>
2021-06-11 17:54:35 -0300

    "adicionando testfor.js"

---
+++ Aula 06/TestFor.js
@@ -0,0 +1,25 @@
+//O for precisa de 3 atributos: o primeiro define a variável a ser trabalhada, o segundo a condição limite para a repetição e o último é a ação realizada a cada loop
+
+for (let paginas = 1; paginas <= 10; paginas++) {
+    console.log(`Página atual ${paginas}`)
+}
+
+// //Micro-desafios 1
+
+for (let contador = 1; contador <=5; contador++) {
+    console.log(`Hello World`)
+}
+
+// //Micro-desafios 2
+
+for(let contador = 1; contador<=10; contador+=2){
+    console.log(contador)
+}
+
+//Micro-desafios 3 tabuada de 5
+
+// let contador = 0
+// let tabuada = 7
+// for (contador; contador<=10;contador++){
+//     console.log(contador)
+// }
