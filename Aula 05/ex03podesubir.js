518ff21297417a5f54a661cd02d5589fefc8888b
JonathanCout <jqcoutinho.eng@gmail.com>
2021-06-11 09:39:26 -0300

    "mudanças na msg de resposta"

--- Aula 05/ex03podesubir.js
+++ Aula 05/ex03podesubir.js
@@ -1,18 +1,30 @@
 //Exercicio PodeSubir
 function podeSubir(altura,acompanhada) {
     // Condição padrão
-    if (altura > 1.4 && altura < 2){
+    if (altura > 140 && altura < 200){
         return "Pode subir"
     // Condição com acompanhante
-    } else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "sim" ) {
+    } else if (altura >= 120 && altura <= 140 && acompanhada) {
         return "Pode subir por estar acompanhada"
     // Condição sem acompanhante
-    } else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "não") {
-        return "Necessita de acompanhante para subir"
+    } else if (altura >= 120 && altura <= 140 && acompanhada==false) {
+        return "Não pode subir, necessita de acompanhante"
     // Condição falha para <1.2m e >2m
     } else {
-        return "Não pode subir"
+        return `Sua altura é ${altura}cm. Você não pode subir`
     } 
 }
 
-console.log(podeSubir(1.3,"sim"))
+console.log(podeSubir(130,false))
+console.log(podeSubir(130,true))
+console.log(podeSubir(199,false))
+console.log(podeSubir(200,false))
+console.log(podeSubir(119,false))
+console.log(podeSubir(150,false))
+
+//Jeito melhor de mostrar o acompanhada seria usar só o boolean true ou false, o jeito antigo ta comentado aqui embaixo
+//  else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "sim" ) {
+//     return "Pode subir por estar acompanhada"
+// // Condição sem acompanhante
+// } else if (altura >= 1.2 && altura <= 1.4 && acompanhada == "não") {
+//     return "Necessita de acompanhante para subir"
